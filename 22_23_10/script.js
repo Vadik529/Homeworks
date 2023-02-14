b49d13274969fed410509d071ace556276682436
Vadik529 <vadim.kobalt.vereshchak.9932@gmail.com>
2023-02-14 16:12:24 +0200

    fixed hw22

--- 22_23_10/script.js
+++ 22_23_10/script.js
@@ -1,43 +1,46 @@
 const form = document.getElementById("mainForm");
 
 class InputParent {
-  constructor(name, type) {
+  constructor(name, type, id) {
     this.name = name;
     this.type = type;
+    this.id = id;
   }
 
-  createInput(attr) {
+  createInput() {
     this.element = document.createElement("input");
+    this.element.name = this.name;
     this.element.type = this.type;
-    this.element.setAttribute(attr, this.attrValue);
   }
+
   createLabel(text) {
     const label = document.createElement("label");
+
     label.textContent = text;
     label.setAttribute("for", this.id);
     label.prepend(this.element);
+
     form.append(label);
   }
 }
 
 class Input extends InputParent {
-  constructor(name, type) {
-    super(name, type);
+  constructor(name, types, id) {
+    super(name, types, id);
   }
   addPlaceholder(placeholder) {
     this.element.placeholder = placeholder;
   }
 }
 
-const input = new Input("input", "input");
+const input = new Input("input", "input", Date.now());
 input.createInput();
 input.addPlaceholder("First input");
 input.createLabel("Input your text");
 
 class Checkbox extends InputParent {
-  constructor(name, type, id) {
-    super(name, type);
-    this.id = id;
+  constructor(name, types, id) {
+    super(name, types, id);
   }
 }
 
@@ -46,16 +49,15 @@ checkbox.createInput();
 checkbox.createLabel("Checked this checkbox");
 
 class Radio extends InputParent {
-  constructor(name, type, id) {
-    super(name, type);
-    this.id = id;
+  constructor(name, types, id) {
+    super(name, types, id);
   }
 }
 
-const radio = new Radio("radio", "radio", Date.now());
+const radio = new Radio("contact", "radio", Date.now());
 radio.createInput();
 radio.createLabel("Select radio");
 
-const radio1 = new Radio("radio", "radio", Date.now());
+const radio1 = new Radio("contact", "radio", Date.now());
 radio1.createInput();
 radio1.createLabel("Select another radio");
