807ea48dfdc7f5355df0d03a90848e5bfb497206
Vadik529 <vadim.kobalt.vereshchak.9932@gmail.com>
2023-02-20 21:26:57 +0200

    fixed fetch

--- 21_17_10/index.js
+++ 21_17_10/index.js
@@ -1,97 +1,76 @@
-const link = "https://fakestoreapi.com/products";
-const wrapper = document.getElementById("wrapper");
-const select = document.getElementById("select");
-let optionSet = new Set();
-
-function filter(card) {
-  return function sort(e) {
-    const todos = card.childNodes;
-    todos.forEach(function (todo) {
-      switch (e.target.value) {
-        case "men's clothing":
-          if (todo.getAttribute("data-category") == "men's clothing") {
-            todo.style.display = "block";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-
-        case "jewelery":
-          if (todo.getAttribute("data-category") == "jewelery") {
-            todo.style.display = "block";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-
-        case "electronics":
-          if (todo.getAttribute("data-category") == "electronics") {
-            todo.style.display = "block";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-        case "women's clothing":
-          if (todo.getAttribute("data-category") == "women's clothing") {
-            todo.style.display = "block";
-          } else {
-            todo.style.display = "none";
-          }
-          break;
-
-        case "all":
-          todo.style.display = "block";
-      }
-    });
-  };
-}
-
-fetch(link)
+fetch("https://fakestoreapi.com/products/categories")
   .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to get categories from API");
+    }
     return response.json();
   })
-  .then((data) => {
-    data.forEach((element) => {
-      createContent(element);
-    });
-    data.forEach((element) => {
+  .then((categories) => {
+    const categorySelect = document.createElement("select");
+    categorySelect.className = "form-select my-3";
+
+    categories.forEach((category) => {
       const option = document.createElement("option");
-      option.setAttribute("value", element.category);
-      option.textContent = element.category;
-      if (optionSet.has(option.textContent)) {
-        return;
-      } else {
-        optionSet.add(option.textContent);
-        select.append(option);
-        console.log(optionSet);
-      }
+      option.value = category;
+      option.textContent = category;
+      categorySelect.appendChild(option);
+    });
+
+    document.getElementById("product-container").appendChild(categorySelect);
+
+    const productContainer = document.createElement("div");
+
+    productContainer.className = "row row-cols-1 row-cols-md-3 g-4";
+    productContainer.id = "product-container";
+    document.getElementById("product-container").appendChild(productContainer);
+
+    categorySelect.addEventListener("change", () => {
+      const selectedCategory = categorySelect.value;
+
+      fetch(`https://fakestoreapi.com/products/category/${selectedCategory}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(
+              `Failed to get products for category ${selectedCategory} from API`
+            );
+          }
+          return response.json();
+        })
+        .then((products) => {
+          productContainer.innerHTML = "";
+
+          products.forEach((product) => {
+            const card = createProductCard(product);
+            productContainer.appendChild(card);
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          alert(
+            `Failed to get products for category ${selectedCategory} from API: ${error}`
+          );
+        });
     });
   })
-  .catch((error) => console.error(error.message));
+  .catch((error) => {
+    console.error(error);
+    alert(`Failed to get categories from API: ${error}`);
+  });
 
-function createContent(el) {
-  const cardWrapper = document.createElement("div");
-  cardWrapper.classList.add("cardWrapper");
-
-  const header = document.createElement("h1");
-  header.innerText = el.title;
-  header.classList.add("header");
-
-  const img = document.createElement("img");
-  img.setAttribute("src", el.image);
-  img.classList.add("img");
-
-  const category = document.createElement("p");
-  category.innerText = `Category: ${el.category}`;
-  cardWrapper.setAttribute("data-category", el.category);
-
-  const price = document.createElement("p");
-  price.innerText = el.price;
-  cardWrapper.append(header);
-  cardWrapper.append(img);
-  cardWrapper.append(category);
-  cardWrapper.append(price);
-  wrapper.append(cardWrapper);
+function createProductCard(product) {
+  const card = document.createElement("div");
+  card.className = "col";
+  card.innerHTML = `
+    <div class="card h-100">
+      <img src="${product.image}" class="card-img-top" alt="${product.title}">
+      <div class="card-body">
+        <h5 class="card-title">${product.title}</h5>
+        <p class="card-text">${product.description}</p>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="price">$${product.price}</div>
+        </div>
+      </div>
+    </div>
+  `;
+  return card;
 }
-
-select.addEventListener("change", filter(wrapper));
